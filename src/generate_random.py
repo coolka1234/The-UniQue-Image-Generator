414b4b75967d6d88007037885526c6c304b3d228
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-10-04 16:08:37 +0200

    customizing

---
+++ src/generate_random.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+raw_data = {
+    "jsonrpc": "2.0",
+    "method": "generateIntegers", #ints
+    "params": {
+        "apiKey": "your-api-key",
+        "n": 6, # number of random integers
+        "min": 1, # minimum value
+        "max": 6, # maximum value
+        "replacement": True # allow duplicates
+    },
+    'id':1
+}
+
+headers = {'Content-type': 'application/json','Content-Length': '200', 'Accept': 'application/json'}
+data=json.dumps(raw_data)
+
+response = requests.post(
+    url='https://api.random.org/json-rpc/2/invoke',
+    data=data,
+    headers=headers
+    )
+
+print(response.text)
