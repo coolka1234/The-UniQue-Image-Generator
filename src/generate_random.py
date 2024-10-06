c555e9d33601adbe0ce0c57edb157c1ee2a82282
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-10-06 13:56:54 +0200

    functionalized

--- src/generate_random.py
+++ src/generate_random.py
@@ -5,28 +5,33 @@ import os
 
 load_dotenv()
 
-raw_data = {
+
+# json_response = response.json()
+# python_response = json_response['result']['random']['data']
+# print(python_response)
+
+def generate_random(number_of_random_integers, minimum_value, maximum_value, duplicates_allowed):
+    raw_data = {
     "jsonrpc": "2.0",
     "method": "generateIntegers", #ints
     "params": {
         "apiKey": os.getenv('RANDOM_API_KEY'),
-        "n": 600, # number of random integers
-        "min": 1, # minimum value
-        "max": 1299, # maximum value
-        "replacement": True # allow duplicates
+        "n": number_of_random_integers, # number of random integers
+        "min": minimum_value, # minimum value
+        "max": maximum_value, # maximum value
+        "replacement": duplicates_allowed # allow duplicates
     },
     'id':1
 }
 
-headers = {'Content-type': 'application/json','Content-Length': '200', 'Accept': 'application/json'}
-data=json.dumps(raw_data)
+    headers = {'Content-type': 'application/json','Content-Length': '200', 'Accept': 'application/json'}
+    data=json.dumps(raw_data)
 
-response = requests.post(
-    url='https://api.random.org/json-rpc/2/invoke',
-    data=data,
-    headers=headers
-    )
-
-json_response = response.json()
-python_response = json_response['result']['random']['data']
-print(python_response)
+    response = requests.post(
+        url='https://api.random.org/json-rpc/2/invoke',
+        data=data,
+        headers=headers
+        )
+    json_response = response.json()
+    python_response = json_response['result']['random']['data']
+    return python_response
