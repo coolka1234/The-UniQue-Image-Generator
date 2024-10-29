6e9a679267d96186b663a06e66ec20b64834fb2b
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-10-29 19:53:34 +0100

    added rectangle

--- src/create_image.py
+++ src/create_image.py
@@ -1,20 +1,26 @@
+from cmath import rect
+from matplotlib.pylab import rec
 import numpy as np
 import matplotlib.pyplot as plt
 from generate_random import generate_random
 
 
-random_data = generate_random(100, 0, 10, True) 
+random_data = generate_random(103, 0, 10, True) 
 print(random_data)
 
 fig, ax = plt.subplots(figsize=(6,6))
 
 for i in range(0, 100, 2):
-    x = random_data[i] * 10  
-    y = random_data[i+1] * 10
+    circle_center_x = random_data[i] * 10  
+    circle_coordinate_y = random_data[i+1] * 10
     size = np.random.rand() * 2000 
+    rectangle_center_x = random_data[i+2] * 10
+    rectangle_coordinate_y = random_data[i+3] * 10
     color = np.random.rand(3,)  
-    circle = plt.Circle((x, y), size/200, color=color, alpha=0.5)  
+    circle = plt.Circle((circle_center_x, circle_coordinate_y), size/200, color=color, alpha=0.5)  
+    square = plt.Rectangle((rectangle_center_x, rectangle_coordinate_y), size/200, size/200, color=color, alpha=0.5)
     ax.add_artist(circle)
+    ax.add_artist(square)
 
 ax.set_xlim(0, 100)
 ax.set_ylim(0, 100)
